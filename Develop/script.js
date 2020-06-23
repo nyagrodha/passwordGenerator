0d2e8166b2afad35617cc38a52d6bd920c9c7152
nyagrodha <nyagrodha@me.com>
2020-06-22 22:15:13 -0500

    added some variable decs

--- Develop/script.js
+++ Develop/script.js
@@ -10,8 +10,29 @@ var specialChar = ["!", "@", "*", "<", ">", "?", ",", "~", "-", ".", "%", "&", "
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// variable declaration
+
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
 // Write password to the #password input
 function writePassword() {
+  var confirmLength = (prompt("How many characters would you like this password to contain?"));
+
+  // while loop to ensure parameters
+  while(confirmLength <= 7 || confirmLength > 128) {
+    alert("The password must be between 8 and 128 characters. Please choose a different number of characters.");
+    var confirmLength = (prompt("How many characters would you like this password to contain?"));
+  }
+
+  //alert the user to how many characters selected
+    alert("Your password will contain ${confirmLength} characters.");
+  // determine parameters
+
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
